ad6e96bad70abefe8b79405b08b10ca64b45b64d
Makiniks <nicolas.makiniks20@gmail.com>
2026-03-01 14:20:18 -0300

    Conhecendo os tipos primitivos em JavaScript
    Diferente da linguagem C, JavaScript engloba os tipos primitivos em um único tipo, chamado de "primitive". Os tipos primitivos em JavaScript são:
    - String: representa uma sequência de caracteres como "Olá, mundo!".
    - Number: representa um valor numérico como 42, 3.14, NaN e Infinity.
    - Boolean: representa um valor lógico, que pode ser verdadeiro (true) ou falso (false).
    - Object: representa uma coleção de propriedades e métodos, como arrays, funções e objetos literais.
    - Undefined: representa uma variável que foi declarada, mas ainda não foi atribuída a nenhum valor.

---
+++ ex002.js
@@ -0,0 +1,17 @@
+let n = 200;
+
+console.log(typeof n);
+
+n = "Google";
+
+console.log(typeof n);
+console.log(typeof 6);
+console.log(typeof 6.5);
+console.log(typeof "6.5");
+console.log(typeof []);
+console.log(typeof {});
+console.log(typeof function () {});
+console.log(typeof undefined);
+console.log(typeof NaN);
+console.log(typeof Infinity);
+console.log(typeof null);
